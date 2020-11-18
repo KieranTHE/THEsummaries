07d396d9eaff28a38aad02570e87c2132c27ceee
Kieran <kieran@truehybridenergy.com>
2020-11-18 17:54:54 +1000

    initial commit

---
+++ toggl_interface.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import logging
+import base64
+import sys
+import pandas as pd
+
+#set logging
+logging.basicConfig(level=logging.DEBUG)
+
+#read secrets
+with open('secrets.json') as secrets_file:
+  secrets = json.load(secrets_file)
+logging.debug(secrets['toggl_api_key'])
+
+#request detailed report from toggl
+string_api = secrets['toggl_api_key'] + ':' + "api_token"
+
+headers = {
+    'Authorization' : 'Basic '+ base64.b64encode(string_api.encode('ascii')).decode("utf-8"),
+}
+
+params={'user_agent':secrets['email'],'workspace_id':'4435390'}
+
+
+#me details
+url='https://www.toggl.com/api/v8/me'
+response=requests.get(url,headers=headers)
+if response.status_code!=200:
+    logging.error(response.text)
+    quit()
+
+response=response.json()
+logging.debug(response)
+email=response['data']['email']
+logging.debug(email)
+
+#reports
+url='https://toggl.com/reports/api/v2/details'
+
+response=requests.get(url,headers=headers,params=params)
+if response.status_code!=200:
+    logging.error(response.text)
+    quit()
+
+response=response.json()
+response_df = pd.json_normalize(response['data'])
+#logging.debug(response_df['project'])
+response_df.rename(
+    columns={
+        'client':'CLIENT',
+        'project':'PROJECT',
+        'dur':'HOURS'
+        }, 
+    inplace=True)
+grouped_df = response_df.groupby(['CLIENT','PROJECT'])['HOURS'].sum()
+result_df = grouped_df.div(3600000).round(decimals=2)
+logging.debug(result_df)
+result_df.to_csv('result.csv')
